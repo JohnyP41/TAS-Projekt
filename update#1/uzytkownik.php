b39675f112701e25cd2740e41214f8cb19b2a4ac
JohnyP41 <plaljahn@gmail.com>
2019-01-18 23:02:12 +0100

    Add files via upload

--- update#1/uzytkownik.php
+++ update#1/uzytkownik.php
@@ -82,6 +82,62 @@ if(!isset($_SESSION['zalogowany']))
 <body>
 
 <?php
+ 
+$dataPoints = array();
+//Best practice is to create a separate file for handling connection to database
+try{
+     // Creating a new connection.
+    // Replace your-hostname, your-db, your-username, your-password according to your database
+    $link = new \PDO(   'mysql:host=localhost;dbname=kandydaci;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
+                        'root', //'root',
+                        '', //'',
+                        array(
+                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                            \PDO::ATTR_PERSISTENT => false
+                        )
+                    );
+	
+    $handle = $link->prepare('select id,fname, points from uzytkownicy'); 
+    $handle->execute(); 
+    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
+		
+    foreach($result as $row){
+        array_push($dataPoints, array("x"=> $row->id, "y"=> $row->points,"name"=> $row->fname));
+    }
+	$link = null;
+}
+catch(\PDOException $ex){
+    print($ex->getMessage());
+}
+	
+?>
+ 
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Wyniki"
+	},
+	data: [{
+		type: "pie", //change type to bar, line, area, pie, etc  
+        showInLegend: true,
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+  
+
+<?php
 
 
 
