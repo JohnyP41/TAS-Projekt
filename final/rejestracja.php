cc26355b136355448c101e924919c355dc0d42a8
s430678 <krzysztof_czerwinski@interia.eu>
2019-01-27 22:58:32 +0100

    xd

---
+++ final/rejestracja.php
@@ -0,0 +1,279 @@
+<?php
+	session_start();
+
+	
+//poprawnosc formularza
+	if(isset($_POST['email']))
+	{
+		$works=true;
+		
+$fname = $_POST['fname'];
+		if((strlen($fname)<2) || (strlen($fname)>20))
+		{
+			$works=false;
+			$_SESSION['err_fname']="Imię musi posiadać od 2 do 20 znaków";
+		}
+		
+		if(!preg_match("![^a-zA-ZąĄćĆęĘóÓśŚłŁżŻźŹńŃ]+!i",  $fname))
+		{
+			//echo "poprawne znaki";
+		}
+		else
+		{
+			$works=false;
+			$_SESSION['err_fname']="Można używać jedynie liter";
+		}
+		
+		
+$lname = $_POST['lname'];
+		if((strlen($lname)<2) || (strlen($lname)>20))
+		{
+			$works=false;
+			$_SESSION['err_lname']="Nazwisko musi posiadać od 2 do 20 znaków";
+		}
+		
+		if(!preg_match("![^a-zA-ZąĄćĆęĘóÓśŚłŁżŻźŹńŃ]+!i",  $lname))
+		{
+			//echo "poprawne znaki";
+		}
+		else
+		{
+			$works=false;
+			$_SESSION['err_lname']="Można używać jedynie liter";
+		}
+$email = $_POST['email'];
+		$emailB = filter_var($email,FILTER_SANITIZE_EMAIL);
+		
+		if((filter_var($emailB,FILTER_VALIDATE_EMAIL)==false) || ($emailB!=$email))
+		{
+			$works=false;
+			$_SESSION['err_email']="Niepoprawny E-mail"	;
+		}
+		
+		
+		
+		
+		
+$user = $_POST['user'];
+		if((strlen($user)<6) || (strlen($user)>20))
+		{
+			$works=false;
+			$_SESSION['err_user']="Login musi posiadać od 6 do 20 znaków";
+		}
+		
+		if(!preg_match("![^a-zA-Z0-9]+!i",  $user))
+		{
+			//echo "poprawne znaki";
+		}
+		else
+		{
+			$works=false;
+			$_SESSION['err_user']="Można używać jedynie liter(Bez polskich znaków) i cyfr";
+		}
+		
+$passA = $_POST['passA'];
+$passB = $_POST['passB'];
+		if((strlen($passA)<6) || (strlen($passB)>20))
+		{
+			$works=false;
+			$_SESSION['err_passA']="Hasło musi posiadać od 6 do 20 znaków";
+
+		}
+		
+		if($passA!=$passB)
+		{
+			$works=false;
+			$_SESSION['err_passB']="Hasła muszą być identyczne";
+			
+		}
+		
+		
+		$pass_hash = password_hash($passA,PASSWORD_DEFAULT);
+		
+		
+		if(!isset($_POST['role']))
+		{
+			$role = "2";
+			$points = 1;
+		}
+		else
+		{
+			$role = "3";
+			$points = 0;
+		}
+		
+		
+		
+		require_once "connect.php";
+		
+		
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		
+		try
+		{
+			$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name, $db_role);
+			if ($polaczenie->connect_errno!=0)
+			{
+				throw new Exception(mysqli_connect_errno());
+			}
+			else
+			{
+				$rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE email='$email'");
+				
+				if(!$rezultat) throw new Exception($polaczenie->error);
+				$howmany_email = $rezultat->num_rows;
+				if($howmany_email>0)
+				{
+		
+					$works=false;
+					$_SESSION['err_email']="Istnieje już konto przypisane do tego adresu Email";
+			
+		
+				}
+				
+				$rezultat = $polaczenie->query("SELECT id FROM uzytkownicy WHERE user='$user'");
+				
+				if(!$rezultat) throw new Exception($polaczenie->error);
+				$howmany_user = $rezultat->num_rows;
+				if($howmany_user>0)
+				{
+		
+					$works=false;
+					$_SESSION['err_user']="Istnieje już konto przypisane do tej nazwy uzytkownika";
+			
+		
+				}
+						
+						
+				if ($works==true)
+				{
+					if($polaczenie->query("INSERT INTO uzytkownicy VALUES(NULL,'$user','$pass_hash','$fname','$lname','$email','$points','$role')"))
+					{
+						$_SESSION['udanarejestracja']=true;
+						header('Location: witamy.php');
+					}	
+					else
+					{
+						throw new Exception($polaczenie->error);
+					}
+				}
+				$polaczenie->close();
+			}
+		
+		}
+		catch(Exception $e)
+		{
+			echo "Błąd serwera.";
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+	}
+	
+	
+?>
+
+<!doctype html>
+
+<html lang="pl">
+<head>
+  <link rel="stylesheet" href="css/styles1.css">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+  <title>Wybory - Rejestracja</title>
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+  <link rel="icon" href="favicon.png">
+<body>
+  <div name="div1">
+	  <h1>Rejestracja</h1><br><br>
+	  <form method="post" align ="center">
+	  <div name="div2">
+		  <br/><input type="text" name=fname placeholder="Imię"><br>
+		  <?php
+			if(isset($_SESSION['err_fname']))
+			{
+				echo "<font color=\"red\">".$_SESSION['err_fname']."<font color=\"black\">";
+				unset($_SESSION['err_fname']);
+			}
+		  ?>
+		  <br>
+		  <br/><input type="text" name=lname placeholder="Nazwisko"><br>
+			<?php
+			if(isset($_SESSION['err_lname']))
+			{
+				echo "<font color=\"red\">".$_SESSION['err_lname']."<font color=\"black\">";
+				unset($_SESSION['err_lname']);
+			}
+		  ?>
+		  <br>
+		  <br/><input type="text" name=email placeholder="E-mail"><br>
+			<?php
+			if(isset($_SESSION['err_email']))
+			{
+				echo "<font color=\"red\">".$_SESSION['err_email']."<font color=\"black\">";
+				unset($_SESSION['err_email']);
+			}
+		  ?>
+		  <br>
+		  <br/><input type="text" name=user placeholder="Login"><br>
+			<?php
+			if(isset($_SESSION['err_user']))
+			{
+				echo "<font color=\"red\">".$_SESSION['err_user']."<font color=\"black\">";
+				unset($_SESSION['err_user']);
+			}
+		  ?>
+		  <br>
+		  <br/><input type="password" name='passA' placeholder="Hasło"><br>
+			<?php
+			if(isset($_SESSION['err_passA']))
+			{
+				echo "<font color=\"red\">".$_SESSION['err_passA']."<font color=\"black\">";
+				unset($_SESSION['err_passA']);
+			}
+		  ?>
+		  <br>
+		  <br/><input type="password" name='passB' placeholder="Powtórz hasło"><br>
+			<?php
+			if(isset($_SESSION['err_passB']))
+			{
+				echo "<font color=\"red\">".$_SESSION['err_passB']."<font color=\"black\">";
+				unset($_SESSION['err_passB']);
+			}
+		  ?>
+		  <br>
+		</div>
+	  <label>
+		<br>Chcę Kandydować: <input type="checkbox" name=role> <br/><br/>
+	  </label>
+	  <br/><input type="submit" value="Załóż konto">
+	  
+	  
+	  
+	  </form>
+  </div>
+</body>
+</head>
+</html>
---
+++ final/uzytkownik.php
@@ -0,0 +1,313 @@
+<?php
+
+function glosowanie()
+{
+//polaczenie do bazy danych
+require_once "connect.php";
+$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+if($polaczenie->connect_errno!=0)
+{
+	echo "Error: nie dziaa.";
+}
+else
+{
+	//ustanowienie zapytania do bazy danych
+	$sql = "SELECT * FROM uzytkownicy WHERE role=1";
+	$sql2 = "SELECT * FROM uzytkownicy WHERE id=1";
+	if($rezultat = @$polaczenie->query($sql2))
+	{	
+		while($row = $rezultat->fetch_assoc())
+		{
+			$glosowanie=$row['points'];
+
+		}
+	}
+	if(($rezultat = @$polaczenie->query($sql))and($glosowanie==1))
+	{
+		$i = 1;
+		//while skacze po wierszach bazy danych
+		while($row = $rezultat->fetch_assoc())
+		{
+			if($i == 1)
+			{
+				echo "<div name = 'table'><table>";
+				echo "<tr><td><b>Imię</b></td><td><b>Nazwisko</b></td><td><b>Decyzja</td></tr>";
+				$i++;
+			}
+			if ($_SESSION['points']>0)
+			{
+			echo "<tr><td>".$row['fname']."</td><td>".$row['lname']."</td>";
+			}
+			else
+			{
+				echo "<tr><td>".$row['fname']."</td><td>".$row['lname']."</td></tr>";
+			}
+			if ($_SESSION['points']>0){
+			echo "<td><form method='post'>";
+			echo "<input type='submit' name=".$row['user']." id=".$row['id']." value='Zagłosuj na tego kandydata' /><br/>";
+			echo "</form></td></tr>";
+			
+				if(array_key_exists($row['user'],$_POST)){
+					$row['points']+=1;
+					if($polaczenie->query('UPDATE uzytkownicy SET points = '.$row["points"].' WHERE id =' .$row["id"]))
+					{	$_SESSION['points'] -=1;
+						$polaczenie->query('UPDATE uzytkownicy SET points = '.$_SESSION["points"].' WHERE id =' .$_SESSION["id"]);
+						header('Location: uzytkownik.php');
+					}
+				}
+			}
+		} 
+	}
+	else 
+	{
+		echo "<div name = 'votingEnd'>Głosowanie zakończonoe.</div>";
+	}
+	
+	
+	
+	$polaczenie->close();
+}
+
+//sprintf('SELECT id FROM uzytkownicy');
+
+
+}
+
+
+
+
+
+function dodawanie()
+{
+//polaczenie do bazy danych
+require_once "connect.php";
+$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+if($polaczenie->connect_errno!=0)
+{
+	echo "Error: nie dziaa.";
+}
+else
+{
+	$i = 1;
+	//ustanowienie zapytania do bazy danych
+	$sql = "SELECT * FROM uzytkownicy WHERE role=3";
+	$sql1 = "SELECT * FROM uzytkownicy WHERE id=1";
+	if($rezultat = @$polaczenie->query($sql))
+	{
+		//while skacze po wierszach bazy danych
+		while($row = $rezultat->fetch_assoc())
+		{
+				if($i == 1)
+				{
+					echo "<div name = 'table'><table>";
+					echo "<tr><td><b>Imię</b></td><td><b>Nazwisko</b></td><td><b>Akceptacja</b></td></tr>";
+					$i++;
+				}
+				echo "<tr><td>".$row['fname']."</td><td>".$row['lname'].'</td>';
+				if ($_SESSION['points']>0){
+				echo "<td><form method='post'>";
+				echo "<input type='submit' name=".$row['user']." id=".$row['id']." value='Zaakceptuj kandydata'></input>";
+				echo "</form></td></tr>";
+					if(array_key_exists($row['user'],$_POST)){
+						
+						if($polaczenie->query('UPDATE uzytkownicy SET role = "1" WHERE id =' .$row["id"]))
+						{	
+
+							header('Location: uzytkownik.php');
+						}
+					}
+				}
+				
+		}
+		echo "</table></div>";
+	}
+	if($rezultat = @$polaczenie->query($sql1))
+	{
+		while($row = $rezultat->fetch_assoc())
+		{
+			if($row['points']==0){
+			echo "<div name ='startstop'><form method='post'>";
+			echo "<input type='submit' name=".$row['user']." id=".$row['id']." value='Start głosowania' /><br/>";
+			echo "</form></div>";
+			
+				if(array_key_exists($row['user'],$_POST)){
+					
+					if($polaczenie->query('UPDATE uzytkownicy SET points = 1 WHERE id =1'))
+					{	
+						
+						header('Location: uzytkownik.php');
+					}
+				}
+			
+		
+			}
+			else
+			{
+			echo "<div name ='startstop'><form method='post'>";
+			echo "<input type='submit' name=".$row['user']." id=".$row['id']." value='Stop głosowania' /><br/>";
+			echo "</form></div>";
+			
+				if(array_key_exists($row['user'],$_POST)){
+					
+					if($polaczenie->query('UPDATE uzytkownicy SET points = 0 WHERE id =1'))
+					{	
+						
+						header('Location: uzytkownik.php');
+					}
+				}
+			
+				
+			}
+		}
+		
+	}
+	
+	
+	$polaczenie->close();
+}
+
+//sprintf('SELECT id FROM uzytkownicy');
+
+
+}
+
+
+
+
+
+
+
+
+?>
+
+<?php
+session_start();
+
+if(!isset($_SESSION['zalogowany']))
+{
+	header('Location: index.html');
+	exit();
+}
+?>
+
+
+
+
+
+<!doctype html>
+
+<html lang="pl">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+  <link rel="stylesheet" href="css/styles2.css">
+
+  <title>Wybory</title>
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="icon" href="favicon.png">
+<body>
+
+<?php
+ 
+$dataPoints = array();
+//Best practice is to create a separate file for handling connection to database
+try{
+     // Creating a new connection.
+    // Replace your-hostname, your-db, your-username, your-password according to your database
+    $link = new \PDO(   'mysql:host=localhost;dbname=kandydaci;charset=utf8mb4', //'mysql:host=localhost;dbname=canvasjs_db;charset=utf8mb4',
+                        'root', //'root',
+                        '', //'',
+                        array(
+                            \PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                            \PDO::ATTR_PERSISTENT => false
+                        )
+                    );
+	
+    $handle = $link->prepare('select id,fname, points from uzytkownicy where points>1'); 
+    $handle->execute(); 
+    $result = $handle->fetchAll(\PDO::FETCH_OBJ);
+		
+    foreach($result as $row){
+        array_push($dataPoints, array("x"=> $row->id, "y"=> $row->points,"name"=> $row->fname));
+    }
+	$link = null;
+}
+catch(\PDOException $ex){
+    print($ex->getMessage());
+}
+	
+?>
+  
+<div name='bar'>
+<?php
+
+if($_SESSION['role']=='1')
+{
+	echo "<b>Zarejestrowałeś się jako kandydat.</b>";
+}
+
+if($_SESSION['role']=='2')
+{
+	echo "<div cass='uInfo'><b>Liczba głosów do oddania: ".$_SESSION['points'] ."</b></div>";
+	
+	
+}
+
+if($_SESSION['role']=='0')
+{
+	echo "<div class='uInfo'><b>Witaj Adminie!</b></div>";
+	//startStop();
+	
+}
+
+if($_SESSION['role']=='3')
+{
+	echo "Zarejestrowałeś się jako kandydat. Oczekuj aż Twoja kandydatura zostanie zaakceptowana.<br/><br/>";
+	
+}
+
+echo"<div name='logoutB'><b>Zalogowano jako ".$_SESSION['fname']." ".$_SESSION['lname'].'.<input type="button" value="Wyloguj" name = "logout" onClick="document.location.href=\'logout.php\'" /></div></b><br>';
+//echo $_SESSION['role'];
+
+
+?>
+</div>
+
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	exportEnabled: true,
+	theme: "light1", // "light1", "light2", "dark1", "dark2"
+	title:{
+		text: "Wyniki"
+	},
+	data: [{
+		showInLegend: true,
+		type: "pie", //change type to bar, line, area, pie, etc  
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+<div name='trend' id="chartContainer" style="height: 300px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+<?php
+	if($_SESSION['role']=='0')
+{
+	dodawanie();
+}
+if($_SESSION['role']=='2')
+{
+	glosowanie();
+}
+?>
+</body>
+</head>
+</html>
